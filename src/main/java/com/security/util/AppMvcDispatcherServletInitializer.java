743a35fa5ef8b6c3919f6eed779c86ae0af37e69
luc-dev10 <luc.dev1008@gmail.com>
2022-02-06 12:19:30 -0500

    add configuration

---
+++ src/main/java/com/security/util/AppMvcDispatcherServletInitializer.java
@@ -0,0 +1,22 @@
+package com.security.util;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class AppMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{
+                AppConfig.class
+        };
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+}
