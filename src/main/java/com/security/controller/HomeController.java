6b5e00bfd61034154828ba7e19b89863e62e810f
luc-dev10 <luc.dev1008@gmail.com>
2022-02-06 12:32:27 -0500

    add controllers/views

---
+++ src/main/java/com/security/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String getHome() {
+        return "home";
+    }
+
+    @GetMapping("/login")
+    public String getLogin() {
+        return "login";
+    }
+}
